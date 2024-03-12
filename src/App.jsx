9e9bad3589f158ff8d57082e66bbb1bef93c17b4
Michael Campbell <michael.campbell14@gmail.com>
2024-03-12 17:20:18 -0400

    adding app and main file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './pages/Layout';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Projects = React.lazy(() => import('./pages/Projects'));
+const Todo = React.lazy(() => import('./pages/Todo'));
+const Hockey = React.lazy(() => import('./pages/Hockey'));
+const Skull = React.lazy(() => import('./pages/Skull'));
+const JimChapman = React.lazy(() => import('./pages/JimChapman'));
+const GithubFinder = React.lazy(() => import('./pages/GithubFinder'));
+const FiftyinFifty = React.lazy(() => import('./pages/FiftyInFifty'));
+const Updraft = React.lazy(() => import('./pages/Updraft'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='todo' element={<Todo />} />
+        <Route path='hockey' element={<Hockey />} />
+        <Route path='skull' element={<Skull />} />
+        <Route path='jim-chapman' element={<JimChapman />} />
+        <Route path='github-finder' element={<GithubFinder />} />
+        <Route path='fifty-in-fifty' element={<FiftyinFifty />} />
+        <Route path='updraft' element={<Updraft />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
