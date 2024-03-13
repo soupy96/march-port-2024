ce6e548d6fd73bdcbb69f8df1d410785f5a02266
Michael Campbell <michael.campbell14@gmail.com>
2024-03-13 19:08:42 -0400

    adding a favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,27 @@ function MainPage() {
   return (
     <div>
       <Helmet>
-        <title>Big Mike ✌</title>
+        <title>Big Mike 🤙</title>
+        <meta name='description' content='A young web developer with heart!' />
+        <meta
+          name='keywords'
+          content='web developer, developer, website, react, wordpress'
+        />
+        <meta name='author' content='Michael Campbell' />
+        <meta property='og:title' content='Big Mike 🤙' />
+        <meta
+          property='og:description'
+          content='A young web developer with heart!'
+        />
+        <meta property='og:image' content='https://example.com/image.jpg' />
+        <meta property='og:url' content='https://example.com/my-page' />
+        <meta name='twitter:title' content='My Page Title' />
+        <meta
+          name='twitter:description'
+          content='This is a description of my page'
+        />
+        <meta name='twitter:image' content='https://example.com/image.jpg' />
+        <meta name='twitter:card' content='summary_large_image' />
       </Helmet>
       <Hero />
       <Body />
