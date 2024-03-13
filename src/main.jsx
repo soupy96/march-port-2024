b9ae4101fe97ac82e34ddf90b2a8e4368295595d
Michael Campbell <michael.campbell14@gmail.com>
2024-03-13 19:48:46 -0400

    moving the favicon to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Favicon from 'react-favicon';
 
 import App from './App';
 import './assets/css/index.css';
@@ -9,6 +10,7 @@ import './assets/css/Fonts.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Favicon url='https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/call-me-hand.png' />
       <App />
     </BrowserRouter>
   </React.StrictMode>
